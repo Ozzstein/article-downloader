d7fc11537b0bebf87e491c5d4d72858f0811d989
Muhammad Abdelhamid <34060563+Ozzstein@users.noreply.github.com>
2020-04-30 01:30:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '9.1.1',
 description = 'A module for downloading scientific journal articles',
 author = 'The Synthesis Project',
 author_email = 'synthesisproject@mit.edu',
-url = 'https://github.com/olivettigroup/article-downloader', # use the URL to the github repo
-download_url = 'https://www.github.com/olivettigroup/article-downloader/tarball/9.1.1',
+url = 'https://github.com/Ozzstein/article-downloader', # use the URL to the github repo
+download_url = 'https://github.com/Ozzstein/article-downloader/tarball/9.1.1',
 keywords = ['journal', 'paper', 'article', 'downloader'], # arbitrary keywords
 )
